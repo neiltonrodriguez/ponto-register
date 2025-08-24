b764da606c72ef19a620a7d4613b24e712c0875a
neiltonrodriguez <neiltonrodriguez@gmail.com>
2025-08-24 00:02:15 -0300

    [docker]: ajustando alguns pontos na conteinerização

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -3,6 +3,7 @@
 chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
 chmod -R 775 /var/www/storage /var/www/bootstrap/cache
 
+DB_HOST=${DB_HOST:-mysql}
 echo "Aguardando o MySQL iniciar..."
 until nc -z -v -w30 $DB_HOST 3306
 do
@@ -12,17 +13,14 @@ done
 
 echo "MySQL disponível — executando comandos do Laravel..."
 
-if [ ! -f ".jwt_initialized" ]; then
-  echo "Configurando JWT..."
-  
-  php artisan vendor:publish --provider="Tymon\JWTAuth\Providers\LaravelServiceProvider" --force
-  php artisan jwt:secret --force
-  
-  touch .jwt_initialized
-  echo "JWT configurado com sucesso"
+echo "Ajustando permissões do Laravel..."
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+chmod -R 775 /var/www/storage /var/www/bootstrap/cache
+
+if ! grep -q "APP_KEY=" /var/www/.env || [ -z "$APP_KEY" ]; then
+  php artisan key:generate
 fi
 
-php artisan key:generate
 php artisan migrate --force
 php artisan db:seed --force
 
